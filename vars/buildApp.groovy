9e7d627dc60847215c4148366caf2a562f15d54b
initaman <amandabral2104@gmail.com>
2026-04-16 17:24:49 +0530

    updated keys

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,4 +1,4 @@
 def call(Map args) {
-    echo "App ${args.APP_NAME} with version ${args.APP_VERSION}"
-    echo "Using build tool ${args.BUILD_TOOL} in environment ${args.ENVIRONMENT}"
+    echo "App ${args.appName} with version ${args.appversion}"
+    echo "Using build tool ${args.buildTool} in environment ${args.environment}"
 }
