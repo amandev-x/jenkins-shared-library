4f163b895828695bec5c4dced78bee6b195df5b3
initaman <amandabral2104@gmail.com>
2026-04-16 17:45:12 +0530

    fixed

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,4 +1,4 @@
 def call(Map args) {
-    echo "App ${args.appName} with version ${args.appversion}"
+    echo "App ${args.appName} with version ${args.appVersion}"
     echo "Using build tool ${args.buildTool} in environment ${args.environment}"
 }
