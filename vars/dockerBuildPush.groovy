200f3d42aa2145daf29afb6e553990f19514dae1
initaman <amandabral2104@gmail.com>
2026-04-18 17:36:00 +0530

    fixed

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,7 +1,7 @@
 def call(Map args) {
     withCredentials([usernamePassword(credentialsId: args.credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh "docker image build -t ${args.imageName}:${args.imageTag} ."
-        sh "docker tag ${args.imageName}:latest"
+        sh "docker tag ${args.imageName}:${args.imageTag} ${args.imageName}:latest"
         sh "docker push ${args.imageName}:${args.imageTag}"
         sh "docker push ${args.imageName}:latest"
     }
